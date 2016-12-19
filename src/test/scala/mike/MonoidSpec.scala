767c5ef52bcf5d3755b8cf9a1ccc289a38bab456
michael.long <michael.long@deciphernow.com>
2016-12-19 10:19:22 -0500

    Adds test for multiplication monoid

--- src/test/scala/mike/MonoidSpec.scala
+++ src/test/scala/mike/MonoidSpec.scala
@@ -4,13 +4,21 @@ import mike.Monoid._
 import org.scalacheck.Prop.forAll
 import org.scalacheck.Properties
 
-class MonoidSpec extends Properties("Monoid")  {
+class MonoidSpec extends Properties("Monoid") {
 
   property("sum the list of Ints") = forAll { (ints: List[Int]) =>
-    sum(ints) == (ints.foldLeft(0)((acc, id) => acc + id))
+    sum(ints) == (ints.foldLeft(0)((acc, i) => acc + i))
   }
 
   property("sum the list of strings") = forAll { (strs: List[String]) =>
-    sum(strs) ==  (strs.foldLeft("")((acc, str) => s"$acc$str"))
+    sum(strs) == (strs.foldLeft("")((acc, str) => s"$acc$str"))
+  }
+
+  property("make a local multiplication monoid for Int") = forAll { (ints: List[Int]) =>
+    val mult: Monoid[Int] = new Monoid[Int] {
+      def mappend(a: Int, b: Int): Int = a * b
+      def mzero: Int = 1
+    }
+    sum(ints)(mult) == (ints.foldLeft(1)((acc, i) => acc * i))
   }
 }
