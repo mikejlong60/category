9be467b6f51b5766690a513dc8cad114ef5201dc
michael.long <michael.long@deciphernow.com>
2017-01-24 17:42:16 -0500

    More flailing

--- src/main/haskell/Knight.hs
+++ src/main/haskell/Knight.hs
@@ -17,3 +17,34 @@ in3 start = do
     first <- moveKnight start
     second <- moveKnight first
     moveKnight second
+
+
+in4 :: KnightPos -> [KnightPos]
+in4 start = do
+    first <- moveKnight start
+    second <- moveKnight first
+    third <- moveKnight second
+    moveKnight third
+
+
+in5 :: KnightPos -> [KnightPos]
+in5 start = do
+    first <- moveKnight start
+    second <- moveKnight first
+    third <- moveKnight second
+    fourth <- moveKnight third
+    moveKnight fourth
+
+
+in6 :: KnightPos -> [KnightPos]
+in6 start = do
+    first <- moveKnight start
+    second <- moveKnight first
+    third <- moveKnight second
+    fourth <- moveKnight third
+    fifth <- moveKnight fourth
+    moveKnight fifth
+
+
+canReachIn3 :: KnightPos -> KnightPos -> Bool
+canReachIn3 start end = end `elem` in3 start
