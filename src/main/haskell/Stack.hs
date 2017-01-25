db1fc44771f5afcd462e1c366718e7eb357283f6
michael.long <michael.long@deciphernow.com>
2017-01-25 14:30:47 -0500

    More work on state monad

--- src/main/haskell/Stack.hs
+++ src/main/haskell/Stack.hs
@@ -1,3 +1,5 @@
+import Control.Monad.State
+
 type Stack = [Int]
 
 pop :: Stack -> (Int, Stack)
@@ -12,3 +14,40 @@ stackManip stack = let
     (a, newStack2) = pop newStack1
     in pop newStack2
 
+
+pop2 :: State Stack Int
+pop2 = state $ \(x:xs) -> (x, xs)
+
+push2 :: Int -> State Stack ()
+push2 a = state $ \xs -> ((), a:xs)
+
+stackManip2 :: State Stack Int
+stackManip2 = do
+    push2 3
+    pop2
+    pop2
+
+stackStuff :: State Stack ()
+stackStuff = do
+    a <- pop2
+    if a == 5 then push2 5
+      else do
+        push2 3
+        push2 8
+
+moreStack :: State Stack ()
+moreStack = do
+    a <- stackManip2
+    if a == 100
+        then stackStuff
+        else return ()
+
+
+stackyStack :: State Stack ()
+stackyStack = do
+    stackNow <- get
+    if stackNow == [1,2,3]
+      then put [8,3,1]
+      else put [9,2,1]
+
+      
