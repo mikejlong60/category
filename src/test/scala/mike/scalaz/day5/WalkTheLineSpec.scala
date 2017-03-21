3c520b9a91c0877d4e97ebdf54e54cd4d3fb15ee
michael.long <michael.long@deciphernow.com>
2017-03-21 17:16:01 -0400

    Adds more monadic tests

--- src/test/scala/mike/scalaz/day5/WalkTheLineSpec.scala
+++ src/test/scala/mike/scalaz/day5/WalkTheLineSpec.scala
@@ -21,6 +21,8 @@ class WalkTheLineSpec extends WordSpecLike with Matchers {
       def landRight(n: Birds): Option[Pole] = if (math.abs((right + n) - left) < 4) copy(right = right + n).some
       else none
 
+      def banana: Option[Pole] = none
+
     }
   }
 
@@ -30,6 +32,25 @@ class WalkTheLineSpec extends WordSpecLike with Matchers {
     }
     "not balance if a side differs by more than four birds." in new TestContext {
       Pole(0, 0).landLeft(12) flatMap { _.landRight(2) } should be(empty)
+    }
+    "slip and fall if he steps on the banana even if balanced. " in new TestContext {
+      Pole(0, 0).landLeft(2) flatMap { _.landRight(2) } flatMap { _.banana } should be(empty)
+    }
+    "make functions that ignore their input parameter and just return a predetermined monadic value, in this case none from none >> 3.some" in  {
+      (none: Option[Int]) >> 3.some should be(empty)
+    }
+    "make functions that ignore their input parameter and just return a predetermined monadic value, in this case 3.some from 4.some >> 3.some" in {
+      4.some >> 3.some should be(3.some)
+    }
+    "make functions that ignore their input parameter and just return a predetermined monadic value, in this case none from 4.some >> none" in {
+      4.some >> none should be(empty)
+    }
+    "use `for` syntax" in {
+      val s = for {
+        x <- 3.some
+        y <- "hi".some
+      } yield (x.shows + y)
+      s should be (Some("3hi"))
     }
   }
 }
