6b98f4f63f4da4e1e7573c4d1d3089df98b798c3
michael.long <michael.long@deciphernow.com>
2017-04-04 17:38:26 -0400

    More

---
+++ src/test/scala/mike/scalaz/day6/ReaderSpec.scala
@@ -0,0 +1,42 @@
+package mike.scalaz.day6
+
+import org.scalatest.{Matchers, WordSpecLike}
+
+import scalaz.Scalaz._
+import scalaz.{Monoid, Writer}
+
+class ReaderSpec extends WordSpecLike with Matchers {
+
+  trait TestContext {
+    val f = (_: Int) * 5
+
+    val g = ({ (_: Int) * 2 } |@| { (_: Int) + 10 }) { _ + _ }
+
+    val addStuff: Int => Int = for {
+      a <- (_: Int) * 2
+      b <- (_: Int) + 10
+    } yield a + b
+
+  }
+
+  "Functions are applicative functors, allowing one to operate on eventual results as if they already existed." must {
+    "g applied to 3 is 19." in new TestContext {
+      val a = g(3)
+      val e = 19
+      a should be(e)
+    }
+  }
+
+  "Functions are also monads. Just like other monads, a functor is a value with context. That context is that the value is" +
+    " not present yet and you have to apply the function to something to get its value." must {
+      "in addStuff both *2 and +10 get applied to the number 3. return (a+b) does as well, but it ignores it and always" +
+        "presents a+b as the result.  For this reason the function monad is also called the reader monad because" +
+        "all the functions read from a common source.  Essentially, reader lets you pretend that the value" +
+        "is already there." in new TestContext {
+          val a = addStuff(3)
+          val e = 19
+          a should be(e)
+        }
+    }
+
+}
