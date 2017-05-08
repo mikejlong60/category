5f9d58875bc16539059a375716be760f95cd3925
michael.long <michael.long@deciphernow.com>
2017-05-08 15:24:22 -0400

    Adds tests around some useful monadic functions

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -5,3 +5,5 @@ resolvers += Resolver.url(
 
 //addSbtPlugin("com.github.dnvriend" % "sbt-haskell" % "0.0.8")
 addSbtPlugin("com.typesafe.sbt" % "sbt-git" % "0.8.5")
+
+addSbtPlugin("io.get-coursier" % "sbt-coursier" % "1.0.0-RC2")
---
+++ src/test/scala/mike/scalaz/day8/MonadFlattenSpec.scala
@@ -0,0 +1,34 @@
+package mike.scalaz.day8
+
+import org.scalacheck.Prop.forAll
+import org.scalacheck.Properties
+import scalaz._
+import Scalaz._
+import scala.language.higherKinds
+
+class MonadFlattenSpec extends Properties("Flatten") {
+
+  property("Flatten a nested option") = forAll { (i: Int) =>
+    val a = (Some((i).some): Option[Option[Int]]).join
+    val e = (i).some
+    a == e
+  }
+
+  property("Flatten two lists into one") = forAll { (ints: List[Int]) =>
+    val (a1, a2) = ints.splitAt(ints.length/2)
+    val a = List(a1,a2).join
+    a == ints
+  }
+
+  property("Flatten a nested Either right") = forAll { (i: Int) =>
+    val a = (i).right[String].right[String].join
+    val e = (i).right
+    a == e
+  }
+
+  property("Flatten a nested Either left") = forAll { (i: String) =>
+    val a = (i).left[Int].right[String].join
+    val e = (i).left
+    a == e
+  }
+}
