4fe088b588d2a5783d8499d4b583cdb7c8f3bfe5
michael.long <michael.long@deciphernow.com>
2017-07-12 09:30:10 -0400

    Did MonadTransformer lesson. Could not get last State exmample to compile so deleted it.

--- src/test/scala/mike/scalaz/day10/MonadTransformerSpec.scala
+++ src/test/scala/mike/scalaz/day10/MonadTransformerSpec.scala
@@ -9,14 +9,14 @@ import scalaz._
 class MonadTransformerSpec extends WordSpecLike with Matchers {
 
   trait TestContext {
-    def myName(step: String): Reader[String, String] = Reader {step + ", I am " + _}
+    def myName(step: String): Reader[String, String] = Reader { step + ", I am " + _ }
 
     type ReaderTOption[A, B] = ReaderT[Option, A, B]
     object ReaderTOption extends KleisliInstances with KleisliFunctions {
-      def apply[A, B] (f: A => Option[B]): ReaderTOption[A, B] = kleisli(f)
+      def apply[A, B](f: A => Option[B]): ReaderTOption[A, B] = kleisli(f)
     }
 
-    def configure(key: String) = ReaderTOption[Map[String, String], String] {m => m.get(key)}
+    def configure(key: String) = ReaderTOption[Map[String, String], String] { m => m.get(key) }
 
     def setupConnection = for {
       host <- configure("host")
@@ -24,13 +24,24 @@ class MonadTransformerSpec extends WordSpecLike with Matchers {
       password <- configure("password")
     } yield (host, user, password)
 
+    type StateTReaderTOption[C, S, A] = StateT[({type l[X] = ReaderTOption[C, X]})#l, S, A]
+
+    object StateTReaderTOption extends StateTInstances with StateTFunctions {
+      def apply[C, S, A](f: S => (S, A)) = new StateT[({type l[X] = ReaderTOption[C, X]})#l, S, A] {
+        def apply(s: S) = f(s).point[({type l[X] = ReaderTOption[C, X]})#l]
+      }
+
+      def get[C, S]: StateTReaderTOption[C, S, S] = StateTReaderTOption { s => (s, s)}
+
+      def put[C, S](s: S): StateTReaderTOption[C, S, Unit] = StateTReaderTOption { _ => (s, ())}
+    }
   }
 
   "ReaderMonad" must {
     "read from configuration once" in new TestContext {
-      def localExample: Reader[ String, (String, String, String)] = for {
+      def localExample: Reader[String, (String, String, String)] = for {
         a <- myName("First")
-        b <- myName("Second") >=> Reader {_ + "y"}
+        b <- myName("Second") >=> Reader { _ + "y" }
         c <- myName("Third")
       } yield (a, b, c)
       val a = localExample("Mike")
@@ -48,14 +59,9 @@ class MonadTransformerSpec extends WordSpecLike with Matchers {
         "password" -> "12234"
       )
 
-      val badConfig = Map(
-        "host" -> "fred.com",
-        "user" -> "mjlong"
-      )
-
       val a = setupConnection(goodConfig)
-      val e = Some(("fred.com","mjlong","12234"))
-      a should be (e)
+      val e = Some(("fred.com", "mjlong", "12234"))
+      a should be(e)
     }
     "combine Reader's ability to read from configuration once with Option's ability to express failure" in new TestContext {
 
@@ -64,7 +70,42 @@ class MonadTransformerSpec extends WordSpecLike with Matchers {
         "user" -> "mjlong"
       )
       val a = setupConnection(badConfig)
-      a should be (empty)
+      a should be(empty)
     }
   }
+
+  "StateTReaderOption" must {
+    "stateT to represent state transfer on top of ReaderTOption" in new TestContext {
+
+      type Stack = List[Int]
+
+      type Config = Map[String, String]
+
+      val pop = StateTReaderTOption[Config, Stack, Int] {
+        case x :: xs => (xs, x )
+      }
+
+
+      def push(x: Int): StateTReaderTOption[Config, Stack, Unit]= {
+        import StateTReaderTOption.get
+        import StateTReaderTOption.put
+        for {
+          xs <- get[Config, Stack]
+          r <- put(x :: xs)
+        } yield r
+      }
+
+      def stackManip: StateTReaderTOption[Config, Stack, Int] =  for {
+        _ <- push(3)
+        a <- pop
+        b <- pop
+      } yield b
+
+      val a = stackManip(List(5,8,2,1))(Map())
+      val e = Some((List(8,2,1),5))
+      a should be (e)
+    }
+
+  }
+
 }
