60bb5741ee0f364db748750a0ce31917a9eb3832
michael.long <michael.long@deciphernow.com>
2017-07-13 12:20:01 -0400

    More on lens

--- src/test/scala/mike/scalaz/day11/LensSpec.scala
+++ src/test/scala/mike/scalaz/day11/LensSpec.scala
@@ -27,13 +27,68 @@ class LensSpec extends WordSpecLike with Matchers {
   }
 
   "Lens" must {
-    "let you move the Turtle around a space with immutable state" in new TestContext {
+    "get the turtle x coordinate using the Kleisli andThen(>=>) operator" in new TestContext {
 
       val t0 = Turtle(Point(2.0, 3.0), 0.0, Color(255.toByte, 255.toByte, 255.toByte))
 
       val e = 2.0
 
       turtleX.get(t0) should be(e)
+    }
+
+    "use the set operator which will always return a new value, not change the original" in new TestContext {
+
+      val t0 = Turtle(Point(2.0, 3.0), 0.0, Color(255.toByte, 255.toByte, 255.toByte))
+      val t1 = turtleX.set(t0, 50.0)
+
+      val newE = 50.0
+      val oldE = 2.0
+
+      turtleX.get(t1) should be(newE)
+      turtleX.get(t0) should be(oldE)
+    }
+
+    "use the mod operator to get the value, apply it to some function and set using the result" in new TestContext {
+
+      val t0 = Turtle(Point(2.0, 3.0), 0.0, Color(255.toByte, 255.toByte, 255.toByte))
+      val t1 = turtleX.mod(x => x + 1.0 , t0)
+
+      val newE = 3.0
+      val oldE = 2.0
+
+      turtleX.get(t1) should be(newE)
+      turtleX.get(t0) should be(oldE)
+    }
+
+    "use the variation to mod that's curried called =>=." in new TestContext {
+      val incX = turtleX =>= {_ + 1.0}
+
+      val t0 = Turtle(Point(2.0, 3.0), 0.0, Color(255.toByte, 255.toByte, 255.toByte))
+      val t1 = incX(t0)
+
+      val newE = 3.0
+      val oldE = 2.0
+
+      turtleX.get(t1) should be(newE)
+      turtleX.get(t0) should be(oldE)
+    }
+
+    "the preceding illustrates State transition. It emulates imperative programming " +
+      "on top of an immutable data structure. Following is another way to write incX. It " +
+      "modifies the portion of the state viewed through the lens and returns its new value." in new TestContext {
+      val incX = for {
+        x <- turtleX %= {_ + 1.0}
+      } yield x
+
+
+      val t0 = Turtle(Point(2.0, 3.0), 0.0, Color(255.toByte, 255.toByte, 255.toByte))
+      val t1 = incX(t0)
+
+      val newE = 3.0
+      val oldE = 2.0
+
+      turtleX.get(t1._1) should be(newE)
+      turtleX.get(t0) should be(oldE)
     }
   }
 }
